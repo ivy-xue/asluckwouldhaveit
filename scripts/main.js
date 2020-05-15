0e578ede6a193a3c6fa1ccab1c5af5c0fd927815
Ivy Xue <ixue@princeton.edu>
2020-05-15 01:16:33 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/* const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!'; */
+
+/* function addClover() { 
+            var c = document.createElement('img'); 
+            c.src = "./images/c01.png";
+            c.position = "absolute";
+            c.left = "100px";
+            c.top = "200px";
+            document.getElementById('body').appendChild(c); 
+        }  */
+
